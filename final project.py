ae956f3e7ea9f284e00f0efa4c5dc66fd3a5e0ee
Jchen200 <josephine_chen@brown.edu>
2023-05-01 22:01:27 -0400

    Josephine (0.5 hr): created deck and shuffled deck using random

--- final project.py
+++ final project.py
@@ -1,4 +1,20 @@
+import random
+import tkinter as tk
+
 colors = ["red", "yellow", "green", "blue"]
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 action_cards = ["skip", "reverse", "draw two"]
 wild_cards = ["wild", "wild draw four"]
+
+deck = []
+for color_type in colors:
+   for number_type in numers[1:]:
+       deck.append(str(number_type)+""+color_type)
+    for action_card_type in action_cards:
+        deck.append(action_card_type +""+color_type)
+    for wild_card_type in wild_cards:
+        deck.append(wild_card_type)
+
+random.shuffle(deck)
+
+
